cbafafd0ad262ef8535ee7891162043ef81f25a0
Tejas Rawal <trawal90@gmail.com>
2013-12-12 16:34:39 -0600

    Add #audience_rating which parses through api return for title. Add tests for method which pass

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,11 +3,21 @@ class Movie < ActiveRecord::Base
   has_many :roles
   has_many :stars, through: :roles
 
+  validates :title, presence: true
+
   def rotten_finder
     RottenMovie.find(title: title, limit: 1)
   end
   
   def snippet
     description.truncate 50
+  end
+
+  def audience_rating
+    if rotten_finder
+      rotten_finder.ratings.audience_score
+    else
+      return "Sorry, we could not find your movie."
+    end
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Movie do
 
   let(:movie) {Movie.new}
+  let(:rotten_finder_return) { OpenStruct.new(ratings: OpenStruct.new(audience_score: 84)) }
 
   describe "#snippet" do
     context "When description is less than or equal to 50 chars" do
@@ -17,6 +18,30 @@ describe Movie do
         allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.")
 
         expect(movie.snippet).to eq "When a Roman general is betrayed and his family..."
+      end
+    end
+    context "When description is not given" do
+      it "returns an empty string" do
+        allow(movie).to receive(:description).and_return("")
+
+        expect(movie.snippet).to eq ""
+      end
+    end
+  end
+
+  describe "#audience_rating" do
+    context "when Movie is found on Rotten Tomatoes" do
+      it "return the audience score for the Movie" do
+        allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+        expect(movie.audience_rating).to eq 84
+      end
+    end
+    context "When Movie is not found on Rotten Tomatoes" do
+      it "returns a message stating the movie could not be found" do
+        allow(movie).to receive(:rotten_finder).and_return(nil)
+
+        expect(movie.audience_rating).to eq "Sorry, we could not find your movie."
       end
     end
   end
