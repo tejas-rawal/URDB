f0987257c76eacad93c81eab89ea230949cd7485
Tejas Rawal <trawal90@gmail.com>
2013-12-12 15:38:44 -0600

    Add spec test for #snippet in movie model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:movie) {Movie.new}
+
+  describe "#snippet" do
+    context "When description is less than or equal to 50 chars" do
+      it "returns the full description" do
+        allow(movie).to receive(:description).and_return("Once-general, now-gladiator seeks revenge in arena")
+
+        expect(movie.snippet).to eq "Once-general, now-gladiator seeks revenge in arena"
+      end
+    end
+    context "When description is greater than 50 chars" do
+      it "return the first 47 chars and appends ..." do
+        allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.")
+
+        expect(movie.snippet).to eq "When a Roman general is betrayed and his family..."
+      end
+    end
+  end
 end
